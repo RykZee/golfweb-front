eb3f4de34b7ad4f1b54e0ad01d685506356b79a6
Sebastian Rykowski Zeerak <szeerak@kth.se>
2020-04-08 21:59:58 +0200

    Angular application communicating through REST API, broad strokes implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const endpoint = 'http://localhost:8080/userapi/users/';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'golfweb-front';
+  userId: number = 0;
+  allUsers: any;
+  user: any;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.http.get(endpoint)
+      .subscribe((response) => {
+        this.allUsers = response;
+      });
+  }
+
+  details(id: number) {
+    this.http.get(endpoint + id)
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
+  edit(id: number) {
+
+  }
+
+  delete(id: number) {
+
+  }
 }
